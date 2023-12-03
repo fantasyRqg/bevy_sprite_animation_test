6c88160af4d3da223981cab0cf73b60b83316189
rqg <ranqingguo318@gmail.com>
2023-12-03 20:27:16 +0800

    anim json parse, not finish

--- crates/swj/src/cocos2d_anim/anim.rs
+++ crates/swj/src/cocos2d_anim/anim.rs
@@ -4,16 +4,14 @@ use bevy::{
     reflect::TypePath,
     utils::BoxedFuture,
 };
-use bevy::asset::AsyncReadExt;
+use bevy::asset::{AssetContainer, AsyncReadExt};
 use bevy::utils::{HashMap, thiserror};
-use plist::Dictionary;
 use serde_json::Value;
 use thiserror::Error;
-use crate::cocos2d_anim::sprite_sheet::{PlistSpriteAssetLoaderError, PlistSpriteFrameAsset};
 
+use crate::cocos2d_anim::sprite_sheet::PlistSpriteFrameAsset;
 
 struct TextureData {
-    size: Vec2,
     plist_file: String,
     px: f32,
     py: f32,
@@ -21,11 +19,11 @@ struct TextureData {
 
 struct DisplayData {
     name: String,
-    texture_data: TextureData,
+    xy: Vec2,
+    scale: Vec2,
 }
 
 struct BoneData {
-    name: String,
     display_data: Vec<DisplayData>,
     translate: Vec3,
     scale: Vec2,
@@ -37,6 +35,13 @@ struct MoveBoneFrameData {
     evt: Option<String>,
     color: Option<Color>,
     di: usize,
+    fi: usize,
+}
+
+struct MoveBoneData {
+    frames: HashMap<String, Vec<MoveBoneFrameData>>,
+    interval: f32,
+    frame_size: usize,
 }
 
 
@@ -86,6 +91,73 @@ impl AssetLoader for Cocos2dAnimAssetLoader {
                 let sprite_sheet = sprite_sheet.get::<PlistSpriteFrameAsset>().unwrap();
                 plist_file_data.insert(plist_file_name, sprite_sheet.clone());
             }
+
+
+            let mut texture_data = HashMap::new();
+
+            for ta in anim_data["texture_data"].as_array().unwrap() {
+                let ta = ta.as_object().unwrap();
+                let name = ta["name"].as_str().unwrap().to_string();
+                let plist_file = ta["plistFile"].as_str().unwrap().to_string();
+                let px = ta["pX"].as_f64().unwrap() as f32;
+                let py = ta["pY"].as_f64().unwrap() as f32;
+                let data = TextureData {
+                    plist_file,
+                    px,
+                    py,
+                };
+
+                texture_data.insert(name, data);
+            }
+
+            let mut bone_data = HashMap::new();
+
+            for bd in anim_data["armature_data"].as_array().unwrap().first().unwrap()["bone_data"].as_array().unwrap() {
+                let bd = bd.as_object().unwrap();
+                let name = bd["name"].as_str().unwrap().to_string();
+                let translate = Vec3::new(
+                    bd["x"].as_f64().unwrap() as f32,
+                    bd["y"].as_f64().unwrap() as f32,
+                    bd["z"].as_f64().unwrap() as f32,
+                );
+                let scale = Vec2::new(
+                    bd["cX"].as_f64().unwrap() as f32,
+                    bd["cY"].as_f64().unwrap() as f32,
+                );
+                let mut display_data = Vec::new();
+
+                for dd in bd["display_data"].as_array().unwrap() {
+                    let dd = dd.as_object().unwrap();
+                    let name = dd["name"].as_str().unwrap().to_string();
+                    let name = name.replace(".png", "");
+
+                    let skin_data = dd["skin_data"].as_array().unwrap().first().unwrap();
+                    let xy = Vec2::new(
+                        skin_data["x"].as_f64().unwrap() as f32,
+                        skin_data["y"].as_f64().unwrap() as f32,
+                    );
+                    let scale = Vec2::new(
+                        skin_data["cX"].as_f64().unwrap() as f32,
+                        skin_data["cY"].as_f64().unwrap() as f32,
+                    );
+
+                    display_data.push(DisplayData {
+                        name,
+                        xy,
+                        scale,
+                    });
+                }
+
+
+                let data = BoneData {
+                    display_data,
+                    translate,
+                    scale,
+                };
+
+                bone_data.insert(name, data);
+            }
+
 
             Ok(CocosAnim2dAsset {})
         })
